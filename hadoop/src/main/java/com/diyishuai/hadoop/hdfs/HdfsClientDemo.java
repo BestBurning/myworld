9f7dfd1a6d6e05dafb735ab3e690ac1a204e4118
bruce <350108081@qq.com>
2018-01-07 08:42:27 +0800

    HdfsClientDemo update

--- hadoop/src/main/java/com/diyishuai/hadoop/hdfs/HdfsClientDemo.java
+++ hadoop/src/main/java/com/diyishuai/hadoop/hdfs/HdfsClientDemo.java
@@ -2,22 +2,25 @@ package com.diyishuai.hadoop.hdfs;
 
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Iterator;
+import java.util.Map;
 
 public class HdfsClientDemo {
 
 
     FileSystem fs = null;
 
+    Configuration conf = null;
+
     @Before
     public void init() throws Exception{
-        Configuration conf = new Configuration();
+        conf = new Configuration();
 //        conf.set("fs.defaultFS","hdfs://server01:9000");
 
         fs = FileSystem.get(new URI("hdfs://server01:9000"),conf,"root");
@@ -31,6 +34,60 @@ public class HdfsClientDemo {
     @Test
     public void testDownload() throws IOException {
         fs.copyToLocalFile(new Path("/hihi.txt"),new Path("C:\\Users\\Administrator\\Downloads\\hihi-from-down.txt"));
+    }
+
+    @Test
+    public void testDefaultEnv(){
+        Iterator<Map.Entry<String, String>> iterator = conf.iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, String> entry = iterator.next();
+            System.out.println(entry.getKey()+" : "+entry.getValue());
+        }
+    }
+
+    @Test
+    public void testMkdir() throws IOException {
+        boolean mkdirs = fs.mkdirs(new Path("/test/hello/hadoop"));
+        System.out.println(mkdirs);
+    }
+
+    @Test
+    public void testDeleteDir() throws IOException {
+        boolean delete = fs.delete(new Path("/test/hello"), true);
+        System.out.println(delete);
+    }
+
+    @Test
+    public void testModifyDir() throws IOException {
+        boolean rename = fs.rename(new Path("/test"), new Path("/test123"));
+        System.out.println(rename);
+    }
+
+    @Test
+    public void testLsFiles() throws IOException {
+        RemoteIterator<LocatedFileStatus> files = fs.listFiles(new Path("/"), true);
+        while (files.hasNext()){
+            LocatedFileStatus file = files.next();
+            System.out.println(file.getBlockSize());
+            System.out.println(file.getPath());
+            System.out.println(file.getReplication());
+            System.out.println(file.getOwner());
+            System.out.println("=========");
+        }
+    }
+
+    @Test
+    public void testLsFilesAndDirs() throws IOException {
+        FileStatus[] fileStatuses = fs.listStatus(new Path("/"));
+        for (FileStatus fileStatus : fileStatuses) {
+            System.out.println("path: "+ fileStatus.getPath());
+            System.out.println("name: "+ fileStatus.getPath().getName());
+            System.out.println("Owner : "+fileStatus.getOwner());
+            System.out.println("Permission : "+fileStatus.getPermission());
+            System.out.println("Replication : "+fileStatus.getReplication());
+            System.out.println("Group : "+fileStatus.getGroup());
+            System.out.println("======");
+        }
     }
 
 }

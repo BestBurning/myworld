3a7ec2dd06c09ee5686861f4970c4c9a45d43b8d
bruce <zhushuai026@gmail.com>
2019-10-20 18:38:14 +0800

    CAS ABA Fix

---
+++ concurrent/src/main/java/com/diyishuai/cas/User.java
@@ -0,0 +1,17 @@
+package com.diyishuai.cas;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author: Bruce
+ * @date: 2019-10-20
+ * @description:
+ */
+@ToString
+@Data
+@AllArgsConstructor
+class User{
+    String name;
+}

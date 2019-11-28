74bb09600bf57e2292f6439c3fe7638978e0440f
bruce <zhushuai026@gmail.com>
2019-11-28 17:43:12 +0800

    spring cloud alibaba sentinel

--- spring/spring-cloud/spring-cloud-alibaba/spring-cloud-alibaba-consumer-user/src/main/java/com/diyishuai/cloud/alibaba/consumer/AliConsumerApplication.java
+++ spring/spring-cloud/spring-cloud-alibaba/spring-cloud-alibaba-consumer-user/src/main/java/com/diyishuai/cloud/alibaba/consumer/AliConsumerApplication.java
@@ -3,6 +3,7 @@ package com.diyishuai.cloud.alibaba.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author: Bruce
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.diyishuai.cloud.alibaba")
+@ComponentScan("com.diyishuai.cloud.alibaba")
 public class AliConsumerApplication {
 
     public static void main(String[] args) {
